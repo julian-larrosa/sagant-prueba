beb47032baf608aadca8862557ea97720fa3504b
julidev34 <juli.dev.34@gmail.com>
2026-09-17 11:27:11 -0300

    primer commit

---
+++ src/main/java/com/sagant/prueba/notification_project/NotificationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sagant.prueba.notification_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagant/prueba/notification_project/NotificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagant.prueba.notification_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
